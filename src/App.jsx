0aa5020988b38fa50d6e3e68c6f0f11098a7e43e
Adam Harris <adamtjharris@gmail.com>
2025-04-07 08:27:21 +0200

    add colours

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 import AdamColorForm from "./AdamColorForm";
-
+import { useState } from "react";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+  function handleCreateColor(newColor) {
+    setColors([newColor, ...colors]);
+  }
   return (
     <>
       <h1>Theme Creator</h1>
-      <AdamColorForm/>
-      {initialColors.map((color) => {
+      <AdamColorForm onSubmitColor={handleCreateColor}/>
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
